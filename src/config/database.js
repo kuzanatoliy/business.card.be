e8ddef2c75738cd89a7279c930a6e7c834dd9960
kuzanatoliy <njkznu@gmail.com>
2020-12-01 23:32:27 +0300

    BC BE: add lint configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const DEFAULT_ENV = require('./defautlt');
 
 module.exports = {
-  username: process.env.DB_USERNAME || DEFAULT_ENV.DB_USERNAME,
-  password: process.env.DB_PASSWORD || DEFAULT_ENV.DB_PASSWORD,
   database: process.env.DB_NAME || DEFAULT_ENV.DB_NAME,
+  dialect: process.env.DB_DIALECT || DEFAULT_ENV.DB_DIALECT,
   host: process.env.DB_HOST || DEFAULT_ENV.DB_HOST,
+  password: process.env.DB_PASSWORD || DEFAULT_ENV.DB_PASSWORD,
   port: process.env.DB_PORT || DEFAULT_ENV.DB_PORT,
-  dialect: process.env.DB_DIALECT || DEFAULT_ENV.DB_DIALECT
-}
+  username: process.env.DB_USERNAME || DEFAULT_ENV.DB_USERNAME,
+};
--- src/config/defautlt.js
+++ src/config/defautlt.js
@@ -1,8 +1,8 @@
 module.exports = {
-  DB_USERNAME: "root",
-  DB_PASSWORD: "root",
-  DB_NAME: "business.card.db",
-  DB_HOST: "localhost",
+  DB_DIALECT: 'postgres',
+  DB_HOST: 'localhost',
+  DB_NAME: 'business.card.db', 
+  DB_PASSWORD: 'root',
   DB_PORT: 5432,
-  DB_DIALECT: "postgres"
-}
+  DB_USERNAME: 'root',
+};
